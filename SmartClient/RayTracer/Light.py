5d5d35b4b352b54214ddbcbbb14972be48d23c39
KailenHerrin <108548472+KailenHerrin@users.noreply.github.com>
2024-02-19 17:03:55 -0800

    Delete SmartClient/RayTracer directory

--- SmartClient/RayTracer/Light.py
+++
@@ -1,9 +0,0 @@
-import Color
-import Point
-# Class Light
-class Light: 
-    # Init
-    def __init__(self, name, location, col):
-        self.name = str(name)
-        self.loc = Point.Point(location)
-        self.col = Color.Color(col)
